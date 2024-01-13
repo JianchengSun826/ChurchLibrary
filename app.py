b53d0ee5f25da4c75316c2aceda3388303032fbc
JianchengSun826 <sunjch826@gmail.com>
2024-01-13 11:18:59 -0500

    have to do mail part

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources=r'/*')  # 注册CORS, "/*" 允许访问域名所有api 
  
-# 暂时代替数据库
+
 books = [
     {
         "id":'1',
@@ -24,23 +24,20 @@ books = [
     },
 ]
 app.config['MAIL_SERVER'] = 'smtp.google.com'  
-#  to be fix
+#  to be fix 
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = '.google.com'
 app.config['MAIL_PASSWARD'] = 'passward'
 mail = Mail(app)
 
 
-# 首页
 @app.route('/Library',methods=['get'])
 def index():
-    # 自动在templates里找对应名称的文件
     return render_template('index.html',books=books)
  
 # trun to rent page
 @app.route('/Library/rentbook',methods=['get'])
 def rentbook_html():
-    # 自动在templates里找对应名称的文件
     return render_template('book/rent.html')
 
 # rent
@@ -48,23 +45,20 @@ def rentbook_html():
 def rentbook(): 
     # 获取json数据
     book = request.get_json() 
-    # 用数组下标表示id
     book['id']= str(len(book)-1) 
     print(book['id'])
-    # 添加到books末尾
     books.append(book)
     return jsonify({"books":books})
 
-# mail
-@app.route('/Mail', receiver)
-def mail():
+# # mail
+# @app.route('/Mail', receiver)
+# def mail():
 
-    return
+#     return
 
 
 
 
  
 if __name__ == "__main__":
-    # 运行web服务
     app.run(host='0.0.0.0', port=5000)
